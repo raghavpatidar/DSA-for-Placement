8ab745150fa5af4f7808a5ed40966f922d33901b
LENOVO <raghavlalapatidar@gmail.com>
2021-12-29 17:30:53 +0530

    print lcs

---
+++ DP/3-LCS/05-print_lcs.cpp
@@ -0,0 +1,59 @@
+//--------- Raghav  Patidar---------
+//?  Heading :- Printing Longest common subsequence MEMOIZATION code
+//todo This conde is based on the aditya verma DP concept 
+#include <bits/stdc++.h>
+using namespace std;
+
+int static t[1001][1001];
+
+
+void LCS(string x, string  y, int n, int m) {
+
+    //*  initilization
+    for (int i = 0; i < n + 1; i++)
+        for (int j = 0; j < m + 1; j++)
+            if (i == 0 || j == 0) t[i][j] = 0;
+
+    //* computation
+    for (int i = 1; i < n + 1; i++)
+        for (int j = 1; j < m + 1; j++) {
+            if (x[i - 1] == y[j - 1]) t[i][j] = 1 + t[i - 1][j - 1];
+            else t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+        }
+}
+string printLCS(string x, string y, int n, int m) {
+    string s = "";
+    //*intitial at the end of the metrix and moving upside to find the lcs string
+    int j = m, i = n;
+
+    while (i > 0 and j > 0)
+    {
+        // if two character at ith and jth position are
+        // equal then we simple add them to our resultant string
+        if (x[i - 1] == y[i - 1])  s.push_back(x[i - 1]);
+        // else we will to the position where we came for example
+        // in matrix we have two choice from where we came if two character
+        // are not equal at ith position then we simple move to their parent 
+        // so we can either move to t[i-1][j] or t[i][j-1] or which is max we go to there
+        else {
+            //cheacking max conditon for parent 
+            if (t[i - 1][j] > t[i][j - 1])
+                i--;
+            else
+                j--;
+        }
+    }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+int main()
+{
+    string x, y;
+    int n, m;
+    cin >> n >> m >> x >> y;
+    memset(t, -1, sizeof(t));
+    LCS(x, y, n, m);
+    cout << printLCS(x, y, n, m) << endl;
+    return 0;
+}
---
+++ DP/3-LCS/06-print_lcs_myapproach.cpp
@@ -0,0 +1,51 @@
+//--------- Raghav  Patidar---------
+//?  Heading :- Printing Longest common subsequence MEMOIZATION code
+//todo This conde is based on the aditya verma DP concept 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//* making the dp of string to get the required string directly
+string static t[1001][1001];
+
+
+string  LCS(string x, string  y, int n, int m) {
+
+    //*  initilization complete table with empty string 
+    for (int i = 0; i < n + 1; i++)
+        for (int j = 0; j < m + 1; j++)
+            t[i][j] = "";
+
+    //* computation
+    for (int i = 1; i < n + 1; i++)
+        for (int j = 1; j < m + 1; j++) {
+            //* if two character are equal then we directly add that character to the (i-1 , j-1) th positon string
+            if (x[i - 1] == y[j - 1]) t[i][j] = t[i - 1][j - 1] + x[i - 1];
+            //* otherwise we will make t[i][j] equal to the max of prevoius two string (i-1,j)th or (i,j-1)th
+            else {
+                string a = t[i - 1][j];
+                string b = t[i][j - 1];
+                if (a.size() > b.size()) {
+                    t[i][j] = a;
+                }
+                else {
+                    t[i][j] = b;
+                }
+            }
+        }
+    // returning the final max string after full computation
+    return t[n][m];
+}
+
+
+int main()
+{
+    string x, y;
+    int n, m;
+    cin >> n >> m >> x >> y;
+    memset(t, -1, sizeof(t));
+    cout << LCS(x, y, n, m) << endl;
+
+    return 0;
+}
