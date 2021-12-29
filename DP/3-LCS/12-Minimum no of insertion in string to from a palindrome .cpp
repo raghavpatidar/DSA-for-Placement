f35c0d74b50c34ebcf6d65d713cf4832f2d406a4
LENOVO <raghavlalapatidar@gmail.com>
2021-12-30 02:25:53 +0530

    12-Minimum no of insertion in string to from a palindrome

---
+++ DP/3-LCS/12-Minimum no of insertion in string to from a palindrome .cpp
@@ -0,0 +1,43 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int LCS(string x, string  y, int n, int m) {
+
+        //*  initilization
+        int t[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++)
+            for (int j = 0; j < m + 1; j++)
+                if (i == 0 || j == 0) t[i][j] = 0;
+
+        //* computation
+        for (int i = 1; i < n + 1; i++)
+            for (int j = 1; j < m + 1; j++) {
+                if (x[i - 1] == y[j - 1]) t[i][j] = 1 + t[i - 1][j - 1];
+                else t[i][j] = max(t[i - 1][j], t[i][j - 1]);
+            }
+        return t[n][m];
+    }
+    int longestPalindromeSubseq(string s) {
+        string k = s;
+        reverse(k.begin(), k.end());
+        return LCS(s, k, s.size(), k.size());
+
+    }
+    int countMin(string str) {
+        return str.size() - longestPalindromeSubseq(str);
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.countMin(str) << endl;
+    }
+    return 0;
+}
