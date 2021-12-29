6c77290550cedf210517e636f24c78384b015759
LENOVO <raghavlalapatidar@gmail.com>
2021-12-29 14:08:45 +0530

    lcs top-down code modified

--- Aditya Verma DP/LCS/lcs_top_down.cpp
+++ Aditya Verma DP/LCS/lcs_top_down.cpp
@@ -10,14 +10,14 @@ int static t[1001][1001];
 int LCS(string x, string  y, int n, int m) {
 
     //*  initilization
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < m; j++)
+    for (int i = 0; i < n + 1; i++)
+        for (int j = 0; j < m + 1; j++)
             if (i == 0 || j == 0) t[i][j] = 0;
 
     //* computation
-    for (int i = 1; i < n; i++)
-        for (int j = 1; j < m; j++) {
-            if (x[i - 1] == y[i - 1]) t[i][j] = 1 + t[i - 1][j - 1];
+    for (int i = 1; i < n + 1; i++)
+        for (int j = 1; j < m + 1; j++) {
+            if (x[i - 1] == y[j - 1]) t[i][j] = 1 + t[i - 1][j - 1];
             else t[i][j] = max(t[i - 1][j], t[i][j - 1]);
         }
     return t[n][m];
