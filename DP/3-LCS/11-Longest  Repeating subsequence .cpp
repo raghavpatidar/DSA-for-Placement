d85adfa7e3f40b33a4a3316a259430319588ddff
LENOVO <raghavlalapatidar@gmail.com>
2021-12-30 19:10:11 +0530

    LCS completed

--- DP/3-LCS/11-Longest  Repeating subsequence .cpp
+++ DP/3-LCS/11-Longest  Repeating subsequence .cpp
@@ -1,4 +1,6 @@
 
+// concept :- LCS given one string make a copy into anothert string and fo lcs
+//            with one more condition if the i == j then we will not add that character into out subsequece
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,6 +10,7 @@ public:
 
         int n = x.size();
         int m = n;
+        //! making copy x into y
         string y = x;
         int t[n + 1][m + 1];
         for (int i = 0; i < n + 1; i++)
@@ -17,6 +20,7 @@ public:
         //* computation
         for (int i = 1; i < n + 1; i++)
             for (int j = 1; j < m + 1; j++) {
+                //! we add one new condition i!=j
                 if (i != j && x[i - 1] == y[j - 1]) t[i][j] = 1 + t[i - 1][j - 1];
                 else t[i][j] = max(t[i - 1][j], t[i][j - 1]);
             }
