663bac226abf413525fb0c9872be5cad62daca9e
LENOVO <raghavlalapatidar@gmail.com>
2021-12-30 16:57:57 +0530

    pring scs by adtiya verma

--- DP/2-Unbounded knapsack/03-coindChangeProblem_II.cpp
+++ DP/2-Unbounded knapsack/03-coindChangeProblem_II.cpp
@@ -73,65 +73,65 @@ int main()
 // } Driver Code Ends
 
 
-/*
- ! Aditya verma approach
+
+//  ! Aditya verma approach
 
 
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-class Solution {
-public:
-    int minCoins(int coin[], int m, int sum)
-    {
-        int t[m + 1][sum + 1];
-        //* initilization
-        for (int i = 0;i < sum + 1;i++)t[0][i] = INT_MAX - 1;
-        for (int i = 0;i < m + 1;i++)t[i][0] = 0;
-        for (int i = 1; i < sum + 1; i++) {
-            if (i % coin[0] == 0)t[1][i] = i / coin[0];
-            else t[1][i] = INT_MAX - 1;
-        }
+// class Solution {
+// public:
+//     int minCoins(int coin[], int m, int sum)
+//     {
+//         int t[m + 1][sum + 1];
+//         //* initilization
+//         for (int i = 0;i < sum + 1;i++)t[0][i] = INT_MAX - 1;
+//         for (int i = 0;i < m + 1;i++)t[i][0] = 0;
+//         for (int i = 1; i < sum + 1; i++) {
+//             if (i % coin[0] == 0)t[1][i] = i / coin[0];
+//             else t[1][i] = INT_MAX - 1;
+//         }
 
-        for (int i = 1; i < m + 1; i++)
-        {
-            for (int j = 1; j < sum + 1; j++)
-            {
-                if (coin[i - 1] <= j)
-                {
-                    t[i][j] = min(1 + t[i][j - coin[i - 1]], t[i - 1][j]);
-                }
-                else {
-                    t[i][j] = t[i - 1][j];
-                }
-            }
+//         for (int i = 1; i < m + 1; i++)
+//         {
+//             for (int j = 1; j < sum + 1; j++)
+//             {
+//                 if (coin[i - 1] <= j)
+//                 {
+//                     t[i][j] = min(1 + t[i][j - coin[i - 1]], t[i - 1][j]);
+//                 }
+//                 else {
+//                     t[i][j] = t[i - 1][j];
+//                 }
+//             }
 
-        }
-        return t[m][sum] == INT_MAX ? -1 : t[m][sum];
-    }
+//         }
+//         return t[m][sum] == INT_MAX ? -1 : t[m][sum];
+//     }
 
-};
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int v, m;
-        cin >> v >> m;
+// };
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+//         int v, m;
+//         cin >> v >> m;
 
-        int coins[m];
-        for (int i = 0; i < m; i++)
-            cin >> coins[i];
+//         int coins[m];
+//         for (int i = 0; i < m; i++)
+//             cin >> coins[i];
 
 
-        Solution ob;
-        cout << ob.minCoins(coins, m, v) << "\n";
+//         Solution ob;
+//         cout << ob.minCoins(coins, m, v) << "\n";
 
-    }
-    return 0;
-}
+//     }
+//     return 0;
+// }
 
-*/
+// */
 
